6b289598fa5cd84167517ac3539d65625ed4619a
Eric Pereira <shellshock@pop-os.localdomain>
2021-02-22 20:11:47 -0500

    Got it working so that it takes in the csv input properly. also relative well documented

--- AutoProduction.py
+++ AutoProduction.py
@@ -3,49 +3,54 @@ import csv
 import sys
 import os
 
-filePath = "C:\\Higher_mass_fD_autoProduction.csv";
-print(str(filePath))
+#below is where the filepath is stored, this is where you change the filepath
+csvFileName = "Higher_mass_fD_autoProduction.csv";
+print("filepath is: " + str(csvFileName))
 
-csvFile = open(filePath)
-csvReader = csvFile.read(1024)
-print(csvReader)
+#the csvfile is temporarily opened in order to get the proper dialect
+try:
+    csvFile = open(csvFileName)
+    tempReader = csvFile.read(1024)
+except:
+    print("csv file input not found. Try Again")
+    quit()
 
-dialect = csv.Sniffer().sniff(csvReader)
-reader = csv.reader(csvFile, dialect)
+#dialect is found here, and then 
+dialect = csv.Sniffer().sniff(tempReader)
+csvFile = open(csvFileName)
+csvReader = csv.reader(csvFile, dialect)
 
-for row in reader:
-    print("inside")
-    print(row)
-
-with open(csvFile) as someCSV:
-    dialect = csv.Sniffer().sniff(csvReader)
-    someCSV.seek(0)
-    reader = csv.reader(someCSV, dialect)
-
-input("fuckem bois")
-
+#variables declared below are to be used in upcoming loop through csv
+itemDictionary = {}
 start = []
 end = []
-with open (file, mode='r') as csv_file:
-    for row in data:
-        print(row)
-        if (row[0] == "MZd"):
-            for item in row:
-                itemDictionary.update({item : []})
-        elif row[0] == "MfD1":
-            for item in row:
-                start.append(item)
-        elif row[0] == "":
-            for item in row:
-                end.append(item)
-        else:
-            input("csvfile you input is in wrong format. Try again")
 
-    count = 0
-    for key in itemDictionary:
-        try:
-            itemDictionary[key] = [x for x in range(start[count], end[x]) if x % 5 == 0]
-            count+=1
-        except:
-            input("do not have enough items in start or end. try again")
-            exit()
+#goes through each rwo in the csv and starts organizing data 
+for row in csvReader:
+    if (row[0] == "MZd"):
+        for item in row:
+            if (item != "MZd"):
+                itemDictionary.update({item : []})
+    elif row[0] == "MfD1":
+        for item in row:
+            if(item != "MfD1"):
+                start.append(item)
+    elif row[0] == "":
+        for item in row:
+            if (item != ""):
+                end.append(item)
+    else:
+        print("csvfile you input is in wrong format. Try again")
+        quit()
+
+#goes through each item in the dictionary and gets the start and end range and appends that to the dictionary.      
+count = 0
+for key in itemDictionary:
+    try:
+        itemDictionary[key] = [x for x in range(int(start[count]), int(end[count])) if x % 5 == 0]
+        count+=1
+    except:
+        input("do not have enough items in start or end. try again")
+        exit()
+
+print(itemDictionary)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MindisCernStuff
 This is to automate the job of Mindi so it does 100 things all at once
+
+## Steps Neccesary
+1. open param_card.dat
+2. set the masses for Zd and MfD1
+3. close the card
+4. run: ./bin/generate_events
+5. find the run number that was just created 
+6. go to Events/run#
+7. unzip unweighted_events.tar.gz 
+8. rename the file according to the mass: MZD_XX_XX.lhe
+9. once you have a full coloum make directory with the related runs 
+10. zip it
+11. copy it to my mac
+
